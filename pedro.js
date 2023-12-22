58f6792e8b692e52a7434cf89ec77c87ace2d1ae
pedroGamaz <ph.v.s.g.11@gmail.com>
2023-12-22 07:55:36 -0300

    baseDialog

---
+++ pedro.js
@@ -0,0 +1,10 @@
+console.log("Olá, mundo!");
+let modal = document.getElementById("modal")
+let close = document.getElementById("close")
+close.addEventListener("click", function(){
+    modal.style.display = "none"
+})
+let btn = document.getElementById("btn")
+btn.addEventListener("click", function(){
+    modal.style.display = "flex"
+})
